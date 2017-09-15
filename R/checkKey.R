2268293a67fe8392a0f52cbcf00799773d9b59e4
Marcel Miche <mmiche@ifp-a-232.psycho.unibas.ch>
2017-09-15 13:01:36 +0200

    nothing new

--- R/checkKey.R
+++ R/checkKey.R
@@ -4,4 +4,3 @@
 checkKey <- function(dfList) {
     sapply(dfList, function(x) names(x)[1]=="KEY" & all(is.numeric(x[,1])))
 }
-
--- R/checkKey2.R
+++ R/checkKey2.R
@@ -4,4 +4,3 @@
 checkKey2 <- function(dfList) {
     sapply(dfList, function(x) match("KEY", names(x)))
 }
-
