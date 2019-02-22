f966be09377ad53230b383a33ae32e2c5712f26d
Marcel Miche <mmiche@fp-ws-a232.psycho.unibas.ch>
2019-02-22 12:19:25 +0100

    Total number of functions increased by 1 new function (refPlausible)

--- R/computeDuration.R
+++ R/computeDuration.R
@@ -23,13 +23,13 @@
 #' randSelLs <- randomMultSelection(intolLs[["cleanedDf"]])
 #' # Prerequisites in order to execute computeDuration. End ------------
 #' # -------------------------------------------------------
-#' # Run function 26 of 28; see esmprep functions' hierarchy.
+#' # Run function 27 of 29; see esmprep functions' hierarchy.
 #' # -------------------------------------------------------
 #' # randSelLs[["esRandSelIn"]] is the result of function 'randomMultSelection'.
 #' durDf <- computeDuration(randSelLs[["esRandSelIn"]], RELEVANTVN_ES)
 #' # o=o=o=o=o=o=o=o=o=o=o=o=o=o=o=o=o=o=o=o=o=o=o=o=o=o=o=o=o=o=o=o=o=o
 #
-#' @seealso Exemplary code (fully executable) in the documentation of \code{\link{esmprep}} (function 26 of 28).
+#' @seealso Exemplary code (fully executable) in the documentation of \code{\link{esmprep}} (function 27 of 29).
 #
 #' @export
 #
